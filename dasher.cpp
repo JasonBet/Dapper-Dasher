4b64bbc20e24a83cf307f572a2e7277a08da90c5
Jason B <jasonbetsargon@gmail.com>
2023-10-18 22:07:30 -0700

    added character texture

--- dasher.cpp
+++ dasher.cpp
@@ -14,13 +14,14 @@ int main()
 
     Texture2D scarfy=LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
+    scarfyRec.width=scarfy.width/6;
+    scarfyRec.height=scarfy.height;
+    scarfyRec.x=0;
+    scarfyRec.y=0;
     Vector2 scarfyPos;
+    scarfyPos.x=windowWidth/2-scarfyRec.width/2;
+    scarfyPos.y=windowHeight-scarfyRec.height;
 
-    // rectangle dimensions
-    const int width{50};
-    const int height{80};
-
-    int posY{windowHeight-height};
     int velocity{0};
     
     // is the rectangle in the air
@@ -40,7 +41,7 @@ int main()
         ClearBackground(WHITE);
 
         // perform ground check
-        if(posY>=windowHeight-height)
+        if(scarfyPos.y>=windowHeight-scarfyRec.height)
         {
             // rectangle is on the ground
             velocity=0;
@@ -60,12 +61,14 @@ int main()
         }
 
         // update position
-        posY += velocity; 
+        scarfyPos.y += velocity; 
 
-        DrawRectangle(windowWidth/2,posY,width,height, BLUE);
+        DrawTextureRec(scarfy,scarfyRec,scarfyPos,WHITE);
+
         // stop drawing
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 
     return 0;
